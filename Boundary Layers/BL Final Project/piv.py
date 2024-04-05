bc3266542329f21b7b6a10adcddd6cfa465a48b4
Austin Gardiner <austin.gardiner97@gmail.com>
2024-04-05 17:59:53 -0600

    finished determining x and y values and building u and v grid

--- Boundary Layers/BL Final Project/piv.py
+++ Boundary Layers/BL Final Project/piv.py
@@ -1,17 +1,47 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
-data1 = pd.read_csv('piv-data/B00001.dat', delimiter=' ', header=None, skiprows=3)
-data2 = pd.read_csv('piv-data/B00002.dat', delimiter=' ', header=None, skiprows=3)
-
-for j in range(4):
-    x1 = data1.iloc[:,j]
-    x2 = data2.iloc[:,j]
-
-    diffs = []
-    for i in range(max(len(x1), len(x2))):
-        diff = x1[i] - x2[i]
-        if diff != 0:
-            diffs.append([i, diff])
-    print(diffs)
+start = time.time()
+x = []
+y = []
+v = []
+u = []
+for i in range(1, 1000+1):
+    if i < 10:
+        filename = f'piv-data/B0000{i}.dat'
+    elif i < 100:
+        filename = f'piv-data/B000{i}.dat'
+    elif i < 1000:
+        filename = f'piv-data/B00{i}.dat'
+    else:
+        filename = f'piv-data/B0{i}.dat'
+    data = pd.read_csv(filename, header=None, delimiter=" ", skiprows=3)
+    if len(x) == 0:
+        x_raw = data.iloc[:, 0]
+        for x_val in x_raw:
+            x_adjusted = -1 * x_val
+            if x_adjusted not in x:
+                x.append(x_adjusted)
+    if len(y) == 0:
+        y_raw = data.iloc[:, 1].values
+        for y_val in y_raw:
+            if y_val not in y:
+                y.append(y_val)
+    vx = data.iloc[:, 2].values
+    vy = data.iloc[:, 3].values
+    v_row = []
+    u_row = []
+    for j in range(len(vx)):
+        x_index = len(u_row)
+        if x_index % len(x) == 0 and x_index != 0:
+            v.append(v_row)
+            u.append(u_row)
+            v_row = []
+            u_row = []
+        v_row.append(vx[i])
+        u_row.append(vy[i])
+print(f"Done in {time.time()-start:.2f} s")
+print(f"x: {len(x)}")
+print(f"y: {len(y)}")
